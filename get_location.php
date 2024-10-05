b9286bd33c349ae3d1939b803797588d22f9af12
Alexander David Velasquez Malo <152426567+alexanderdvm@users.noreply.github.com>
2024-10-05 01:29:51 -0500

    Add files via upload

---
+++ get_location.php
@@ -0,0 +1,43 @@
+<?php
+header('Content-Type: application/json'); // Para que el cliente sepa que está recibiendo JSON
+
+// Configuración de la base de datos
+$servername = "alex.cpywocwqwde0.us-east-2.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
+$username = "alex";       // Cambia al usuario de tu base de datos
+$password = "alex1234567890";           // Cambia a la contraseña de tu base de datos
+$dbname = "alex";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+
+// Obtener la última ubicación
+$sql = "SELECT latitud, longitud, timestamp FROM ubicaciones ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+$data = [];
+if ($result->num_rows > 0) {
+    // Obtener los datos de la última ubicación
+    $row = $result->fetch_assoc();
+    
+    // La fecha ya está en el formato correcto
+    $formattedDate = $row['timestamp'];
+    
+    $data = [
+        'latitud' => $row['latitud'],
+        'longitud' => $row['longitud'],
+        'timestamp' => $formattedDate // Utilizar directamente el timestamp formateado
+    ];
+}
+
+// Cerrar la conexión
+$conn->close();
+
+// Enviar datos en formato JSON
+echo json_encode($data);
+?>
